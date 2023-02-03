55bc20e587c0ae16c6ffefa27e9d5b33b9b6a11e
aidookingsley <kaidoog36@gmail.com>
2023-02-03 17:18:17 +0000

    string capacity

---
+++ StringLiterals.cpp
@@ -0,0 +1,8 @@
+//
+//  StringLiterals.cpp
+//  
+//
+//  Created by Kingsley Mensah Aidoo on 2/3/23.
+//
+
+#include "StringLiterals.hpp"
---
+++ StringLiterals.hpp
@@ -0,0 +1,13 @@
+//
+//  StringLiterals.hpp
+//  
+//
+//  Created by Kingsley Mensah Aidoo on 2/3/23.
+//
+
+#ifndef StringLiterals_hpp
+#define StringLiterals_hpp
+
+#include <stdio.h>
+
+#endif /* StringLiterals_hpp */
